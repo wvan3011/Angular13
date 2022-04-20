50fd5c3dcdd81e6dfd0782b5f2dda5278b8cf9ff
wvan3011 <wvan3011@gmail.com>
2022-04-20 11:53:19 +0700

    DataBinding-ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = 'Van';
+  public age = 20;
   constructor() { }
 
   ngOnInit(): void {
