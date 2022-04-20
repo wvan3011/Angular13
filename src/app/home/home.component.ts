c1fc58213784d34b54ef133256d0071832976e47
wvan3011 <wvan3011@gmail.com>
2022-04-20 08:04:32 +0700

    DataBindingValue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public name = 'Van';
   constructor() { }
 
   ngOnInit(): void {
